124e6c20933bf948108c5824f7cde2b1336e61eb
Muneef M <mailmuneef@gmail.com>
2019-11-27 20:47:45 +0530

    Initial Commit

---
+++ AndroidDevices/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AndroidDevices
+//
+//  Created by Muneef M on 27/11/19.
+//  Copyright © 2019 Muneef M. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
